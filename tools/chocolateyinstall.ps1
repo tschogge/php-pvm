89689e359fcdca8dae57c9715bc8d9e58420a238
Giotsche <gioele.petrillo@gmail.com>
2025-06-06 12:42:27 +0200

    changed the scripts

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,20 +1,17 @@
 ﻿
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$packageName = "pvm"
+$packageName = "php-pvm"
+$toolName = "pvm"
 $fileType = "exe"
-$filePath = "$toolsDir/$packageName.$fileType"
-$userDataPath = [Environment]::GetFolderPath('ApplicationData')
-$targetPath = "$userDataPath\.$packageName\bin"
+$userDataPath = [Environment]::GetFolderPath('UserProfile')
+$targetPath = "$userDataPath\.$toolName\bin"
 
 
 if (!(Test-Path $targetPath)) {
     New-Item -ItemType Directory -Path $targetPath -Force
     Write-Host "Created directory $targetPath"
 }
-
-Copy-Item -Path $filePath -Destination $targetPath -Force
-
 
 $currentSystemPath = [Environment]::GetEnvironmentVariable("Path", "Machine")
 if ($currentSystemPath -notlike "*$targetPath*") {
@@ -25,24 +22,13 @@ if ($currentSystemPath -notlike "*$targetPath*") {
     Write-Host "$targetPath is already in the system PATH"
 }
 
-Write-Host "The binary has been installed to $targetPath"
-
-
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'EXE'
-
-  softwareName  = 'pvm*'
-  url           = 'https://github.com/hjbdev/pvm/releases/download/1.2.0/pvm.exe'
-  url64         = 'https://github.com/hjbdev/pvm/releases/download/1.2.0/pvm.exe'
-
-  checksum      = '101AC193D9D40ECFC28964DC6C0090769AAB407C523FEF1105ADB83CB9C78B95E2C70771C42122DB87CF20D467FA40EA90A78B6E88B5F896A67D07356E6165F7'
-  checksumType  = 'sha512'
-  checksum64    = '101AC193D9D40ECFC28964DC6C0090769AAB407C523FEF1105ADB83CB9C78B95E2C70771C42122DB87CF20D467FA40EA90A78B6E88B5F896A67D07356E6165F7'
-  checksumType64= 'sha512'
-
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-  validExitCodes= @(0, 3010, 1641)
+  softwareName  = '$toolName*'
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+  File          = "$toolsDir\$toolName.$fileType"
 }
 
 Install-ChocolateyPackage @packageArgs
